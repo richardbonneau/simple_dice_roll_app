d91ed13c6b738f9cf917c35b1158fcc3bc6484a1
richardbonneau <richardbonneau@hotmail.com>
2019-06-14 16:52:54 -0400

    Replaced my variables holding views with lateinit variables

--- app/src/main/java/com/example/dice_roller/MainActivity.kt
+++ app/src/main/java/com/example/dice_roller/MainActivity.kt
@@ -11,12 +11,15 @@ import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var rollButton:Button
+    lateinit var diceImage:ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val rollButton:Button = findViewById(R.id.roll_button)
-        val diceImage:ImageView = findViewById(R.id.dice_image)
+        rollButton = findViewById(R.id.roll_button)
+        diceImage = findViewById(R.id.dice_image)
 
         fun rollDice(){
             Toast.makeText(this, "button clicked",
