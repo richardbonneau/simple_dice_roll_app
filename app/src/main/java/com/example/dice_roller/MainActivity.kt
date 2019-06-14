b23127e1fceaf8e152341c9f936fbc6ae0ec6436
richardbonneau <richardbonneau@hotmail.com>
2019-06-14 16:33:40 -0400

    Added a dice image instead of displaying a number

--- app/src/main/java/com/example/dice_roller/MainActivity.kt
+++ app/src/main/java/com/example/dice_roller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.dice_roller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -15,27 +16,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton:Button = findViewById(R.id.roll_button)
-        val countUpButton:Button = findViewById(R.id.count_up_button)
+        val diceImage:ImageView = findViewById(R.id.dice_image)
 
-        val diceRolledText: TextView = findViewById(R.id.dice_rolled_text)
         fun rollDice(){
             Toast.makeText(this, "button clicked",
                 Toast.LENGTH_SHORT).show()
 
             val randomInt = Random.nextInt(6)+1
-
-            diceRolledText.text = randomInt.toString()
+            val drawableRessource = when (randomInt){
+                1-> R.drawable.dice_1
+                2-> R.drawable.dice_2
+                3-> R.drawable.dice_3
+                4-> R.drawable.dice_4
+                5-> R.drawable.dice_5
+                else -> R.drawable.dice_6
+            }
+            diceImage.setImageResource(drawableRessource)
         }
 
-        fun countUp(){
-            var currentNum = diceRolledText.text.toString().toInt()
-            currentNum += 1
 
-            diceRolledText.text = currentNum.toString()
-        }
 
         rollButton.setOnClickListener { rollDice() }
-        countUpButton.setOnClickListener { countUp() }
+
 
     }
 }
